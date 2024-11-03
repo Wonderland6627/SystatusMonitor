a89b1056d320294683b946e4cabe45d0d5f0f60c
Wonderland6627 <994015020@qq.com>
2024-11-03 16:19:21 +0800

    添加环形进度条样式

--- FloatWindow.xaml.cs
+++ FloatWindow.xaml.cs
@@ -16,15 +16,60 @@ namespace SystatusMonitor
 {
     /// <summary>
     /// FloatWindow.xaml 的交互逻辑
+    /// 环形进度条样式参考：https://www.cnblogs.com/beatfan/p/13624769.html
     /// </summary>
     public partial class FloatWindow : Window
     {
+        private class CircleProgress
+        {
+            public int edgeLen;
+            public int strokeThickness;
+
+            public Path progressPath;
+
+            public void SetPath(Path path)
+            {
+                progressPath = path;
+                progressPath.Width = edgeLen;
+                progressPath.Height = edgeLen;
+                progressPath.StrokeThickness = strokeThickness;
+            }
+        }
+
+        private CircleProgress cpuProgress;
+        private CircleProgress gpuProgress;
+
         public FloatWindow()
         {
             InitializeComponent();
             SetInitPosition();
 
             this.MouseLeftButtonDown += new MouseButtonEventHandler(DragWindow);
+
+            cpuProgress = new CircleProgress()
+            {
+                edgeLen = 100,
+                strokeThickness = 10,
+            };
+            cpuProgress.SetPath(cpuProgressBar);
+
+            gpuProgress = new CircleProgress()
+            {
+                edgeLen = 70,
+                strokeThickness = 10,
+            };
+            gpuProgress.SetPath(gpuProgressBar);
+
+            double progress = 0;
+            var timer = new System.Windows.Forms.Timer();
+            timer.Tick += (object sender, EventArgs e) =>
+            {
+                progress+=0.01;
+                SetValue(progress, cpuProgress);
+            };
+            timer.Start();
+
+            SetValue(0.33, gpuProgress);
         }
 
         private void SetInitPosition()
@@ -39,6 +84,87 @@ namespace SystatusMonitor
         private void DragWindow(object sender, MouseButtonEventArgs e)
         {
             this.DragMove();
+        }
+
+        /// <summary>
+        /// value: [0, 1]
+        /// </summary>
+        private void SetValue(double value, CircleProgress circleProgress)
+        {
+            double angle = value * 360;
+            //起始点
+            double leftStart = circleProgress.edgeLen / 2;
+            double topStart = circleProgress.strokeThickness;
+            //结束点
+            double endLeft = 0;
+            double endTop = 0;
+            double radius = leftStart - topStart; //环形半径
+            bool isLagreCircle = false; //是否优势弧，即大于180度的弧形
+
+            lbValue.Content = (value * 100).ToString("0") + "%";
+
+            //小于90度
+            if (angle <= 90) //第一象限
+            {
+                double ra = (90 - angle) * Math.PI / 180;
+                endLeft = leftStart + Math.Cos(ra) * radius;
+                endTop = topStart + radius - Math.Sin(ra) * radius;
+            }
+            else if (angle <= 180) //第二象限
+            {
+                double ra = (angle - 90) * Math.PI / 180;
+                endLeft = leftStart + Math.Cos(ra) * radius;
+                endTop = topStart + radius + Math.Sin(ra) * radius;
+            }
+            else if (angle <= 270) //第三象限
+            {
+                isLagreCircle = true;
+                double ra = (angle - 180) * Math.PI / 180;
+                endLeft = leftStart - Math.Sin(ra) * radius;
+                endTop = topStart + radius + Math.Cos(ra) * radius;
+            }
+            else if (angle < 360) //第四象限
+            {
+                isLagreCircle = true;
+                double ra = (angle - 270) * Math.PI / 180;
+                endLeft = leftStart - Math.Cos(ra) * radius;
+                endTop = topStart + radius - Math.Sin(ra) * radius;
+            }
+            else
+            {
+                isLagreCircle = true;
+                endLeft = leftStart - 0.001; //不与起点在同一点，避免重叠绘制出非环形
+                endTop = topStart;
+            }
+
+            Point arcEndPt = new Point(endLeft, endTop);
+            Size arcSize = new Size(radius, radius);
+            ArcSegment arcsegment = new ArcSegment(arcEndPt, arcSize, 0, isLagreCircle, SweepDirection.Clockwise, true);
+
+            PathSegmentCollection pathsegmentCollection = new PathSegmentCollection
+            {
+                arcsegment
+            };
+            PathFigure pathFigure = new PathFigure
+            {
+                StartPoint = new Point(leftStart, topStart),
+                Segments = pathsegmentCollection
+            };
+            PathFigureCollection pathFigureCollection = new PathFigureCollection
+            {
+                pathFigure
+            };
+            PathGeometry pathGeometry = new PathGeometry
+            {
+                Figures = pathFigureCollection
+            };
+
+            if (angle == 360) //达到100%则闭合整个
+            {
+                circleProgress.progressPath.Data = Geometry.Parse(cpuProgressBar.Data.ToString() + " z");
+                return;
+            }
+            circleProgress.progressPath.Data = pathGeometry;
         }
     }
 }
